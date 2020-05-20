8081e635579d943fb0df6eacfa2318e5e56a061f
= <=>
2020-05-20 00:18:19 -0300

    Backend go barber

---
+++ gobarber/backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '0ec26d90205efa7fd23c122de48034aa',
+    expiresIn: '1d',
+  },
+};
---
+++ gobarber/backend/src/server.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import express from 'express';
+import routes from './routes/index';
+
+import './database';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀 App lauched... Port 3333');
+});
